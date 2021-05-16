c4a84f1310e45f0a7a6555b7a7be0b0b91771682
SallemAllem <72173080+SallemAllem@users.noreply.github.com>
2021-05-16 18:04:31 +0600

    Update quada.go

--- quada.go
+++ quada.go
@@ -1,35 +1,43 @@
-package piscine
+package main
 
-import "github.com/01-edu/z01"
+import (
+	"fmt"
+	"github.com/01-edu/z01"
+	"os"
+	"strconv"
+)
 
 func QuadA(x, y int) {
 	if x > 0 && y > 0 {
-		FirstLastRaw(x)
-		if y > 1 {
-			for a := 0; a < y-2; a++ {
-				z01.PrintRune('\n')
-				for b := 0; b < x; b++ {
-					if b == 0 || b == x-1 {
-						z01.PrintRune('|')
-					} else {
-						z01.PrintRune(' ')
-					}
+		for i := 1; i <= y; i++ {
+			for j := 1; j <= x; j++ {
+				if (i == 1 && (j == 1 || j == x)) || (i == y && (j == 1 || j == x)) {
+					z01.PrintRune('o')
+				} else if (j == 1 && (i != 1 || i != y)) || (j == x && (i != 1 || i != y)) {
+					z01.PrintRune('|')
+				} else if i != 1 && j != 1 && i != y && j != x {
+					z01.PrintRune(' ')
+				} else {
+					z01.PrintRune('-')
 				}
 			}
-			z01.PrintRune('\n')
-			FirstLastRaw(x)
-		}
-		if x > 0 && y > 0 {
 			z01.PrintRune('\n')
 		}
 	}
 }
-func FirstLastRaw(x int) {
-	for i := 0; i < x; i++ {
-		if i == 0 || i == x-1 {
-			z01.PrintRune('o')
-		} else {
-			z01.PrintRune('-')
-		}
+func main() {
+	args := os.Args[1:]
+	int1, err := strconv.Atoi(args[0])
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+		os.Exit(2)
 	}
+	int2, err := strconv.Atoi(args[1])
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	QuadA(int1, int2)
 }
